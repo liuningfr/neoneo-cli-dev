bbb9189b687f1cf791eba9a78f6bd571ca448eae
liuning <liuning@kuaishou.com>
2022-05-07 17:16:14 +0800

    update

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -2,6 +2,10 @@
 
 module.exports = exec;
 
+const Package = require('@neoneo-cli-dev/package');
+
 function exec() {
+    const pkg = new Package();
+    console.log(pkg);
     console.log(process.env.CLI_TARGET_PATH, process.env.CLI_HOME_PATH);
 }
---
+++ models/package/README.md
@@ -0,0 +1,11 @@
+# `@neoneo-cli-dev/package`
+
+> TODO: description
+
+## Usage
+
+```
+const package = require('@neoneo-cli-dev/package');
+
+// TODO: DEMONSTRATE API
+```
