bc394ba745970e449f41c045851d01712083bbe5
liuning <liuning@kuaishou.com>
2022-05-19 20:42:58 +0800

    update

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const Command = require('@neoneo-cli-dev/command');
+const log = require('@neoneo-cli-dev/log');
 
 class InitCommand extends Command {
+    init() {
+        this.projectName = this._argv[0] || '';
+        this.force = this._argv[1].force;
+        log.verbose('projectName', this.projectName);
+        log.verbose('force', this.force);
+    }
 
+    exec() {
+        console.log('exec');
+    }
 }
 
 function init(argv) {
