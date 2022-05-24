53f824d4b897a1d6aace813471d4b7d31adb16c0
liuning <liuning@kuaishou.com>
2022-05-24 18:02:35 +0800

    update

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const Command = require('@neoneo-cli-dev/command');
 const log = require('@neoneo-cli-dev/log');
 
@@ -12,7 +14,30 @@ class InitCommand extends Command {
     }
 
     exec() {
-        console.log('exec start');
+        try {
+            this.prepare()
+            
+        } catch (e) {
+           log.error(e.message); 
+        }
+    }
+
+    prepare() {
+        if(!this.isCwdEmpty()) {
+
+        } else {
+
+        }
+    }
+
+    isCwdEmpty() {
+        // or path.resolve('.')
+        const localPath = process.cwd();
+        let fileList = fs.readdirSync(localPath);
+        fileList = fileList.filter((file) => (
+            !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
+        ));
+        return !fileList || fileList.length === 0;
     }
 }
 
