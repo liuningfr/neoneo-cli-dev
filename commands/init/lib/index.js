d3412d4f8cd09a1c074925e3dd4db96491e4936d
liuning <liuning@kuaishou.com>
2022-05-26 17:58:57 +0800

    update

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 const path = require('path');
+const inquirer = require('inquirer');
+const fse = require('fs-extra');
 const Command = require('@neoneo-cli-dev/command');
 const log = require('@neoneo-cli-dev/log');
 
@@ -13,26 +15,51 @@ class InitCommand extends Command {
         log.verbose('force', this.force);
     }
 
-    exec() {
+    async exec() {
         try {
-            this.prepare()
+            const ret = await this.prepare();
+            if (ret) {
+
+            }
             
         } catch (e) {
            log.error(e.message); 
         }
     }
 
-    prepare() {
-        if(!this.isCwdEmpty()) {
-
+    async prepare() {
+        // or path.resolve('.')
+        const localPath = process.cwd();
+        if (!this.isDirEmpty(localPath)) {
+            let ifContinue = false;
+            if (!this.force) {
+                ifContinue = (await inquirer.prompt({
+                    type: 'confirm',
+                    name: 'ifContinue',
+                    default: false,
+                    message: '当前文件夹不为空，是否继续创建项目？'
+                })).ifContinue;
+                if (!ifContinue) {
+                    return
+                }
+            }
+            if (ifContinue || this.force) {
+                const { confirmDelete } = await inquirer.prompt({
+                    type: 'confirm',
+                    name: 'confirmDelete',
+                    default: false,
+                    message: '确定要清空当前文件夹吗？'
+                });
+                if (confirmDelete) {
+                    fse.emptyDirSync(localPath);
+                }
+            }
         } else {
 
         }
     }
 
-    isCwdEmpty() {
-        // or path.resolve('.')
-        const localPath = process.cwd();
+    isDirEmpty(localPath) {
         let fileList = fs.readdirSync(localPath);
         fileList = fileList.filter((file) => (
             !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
